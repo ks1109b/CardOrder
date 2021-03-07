db689990e8d635d89cc9ed56d35d8c6e834365d3
Kseniia Bloshkina <ksenia119@hotmail.com>
2021-03-07 21:01:36 +0300

    Added test

---
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -0,0 +1,23 @@
+
+package ru.netology.web;
+
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+class CallbackTest {
+
+    @Test
+    void shouldSendForm() {
+        open("http://localhost:9999");
+        $("[data-test-id=name] input").setValue("Василий");
+        $("[data-test-id=phone] input").setValue("+79270000000");
+        $("[data-test-id=agreement]").click();
+        $("[type=button] span").click();
+        $("[data-test-id=order-success]").shouldHave(text("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+}
